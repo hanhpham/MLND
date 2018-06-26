733ec5ee2607e7ad9dd5e1342a2d6c66f94ecfa6
Hanh Pham <pham.hanh.m@gmail.com>
2018-06-25 22:39:41 -0700

    feature gathering: reconcile article pub_date with trade_date re holidays, add pub_date for tracking, fix 'is_major/isMajor' keyword issue

--- projects/capstone/features_gather.py
+++ projects/capstone/features_gather.py
@@ -1,6 +1,8 @@
 import os
 import csv
 import json
+from dateutil.parser import parse
+from datetime import datetime, date, timedelta
 
 
 """
@@ -27,11 +29,11 @@ years = [2016]
 months = [12]
 
 
-writer.writerow(['id', 'trade_date', 'headline_senti', 'summary_senti',
+writer.writerow(['id', 'pub_date', 'headline_senti', 'summary_senti',
                  'headline_summary_senti', 'lead_paragraph_senti', 'keyword_in_headline', 
                  'keyword_in_summary', 'keyword_org_isMajor', 'keyword_org_rank',
                  'keyword_org_rank_alt', 'section_name', 'type_of_material',
-                 'print_page', 'word_count'])
+                 'print_page', 'word_count', 'trade_date', 'price'])
 
 for year in years:
     for month in months:
@@ -57,18 +59,20 @@ for year in years:
                 id = unicode(article['_id'])
                 s = sentiments_dict[id]
                 row.append(id)
-                row.append(s['trade_date'])
+                row.append(s['pub_date'])              
                 row.append(s['headline_senti'])
                 row.append(s['summary_senti'])
                 row.append(s['headline_summary_senti'])
                 row.append(s['lead_paragraph_senti'])
                 row.append(s['keyword_in_headline'])
                 row.append(s['keyword_in_summary'])
-                # 'keyword_org_isMajor', 'keyword_org_rank'
                 keywords_org = [keyword for keyword in article['keywords'] if (
                     keyword['name'] == "organizations" and ("Google" in unicode(keyword['value']) or "GOOGLE" in unicode(keyword['value'])))]
                 if len(keywords_org)==1:
-                    row.append(keywords_org[0]['is_major'])
+                    try: #account for article's keyword change: 'is_major' -> 'isMajor'
+                        row.append(keywords_org[0]['is_major'])
+                    except KeyError:
+                        row.append(keywords_org[0]['isMajor'])
                     row.append(keywords_org[0]['rank']) 
                 else:
                     row.append('N')
@@ -78,7 +82,21 @@ for year in years:
                 row.append(article['type_of_material']) 
                 row.append(article['print_page'] if article['print_page'] is not None else 0)
                 row.append(article['word_count'])
-                row.append(prices[s['trade_date']])
+                
+                # if trade date not found in Yahoo's historical stock data, most likely the date fell on a holiday, use the next available trade date 
+                trade_date_found, tries = False, 0
+                trade_date = s['trade_date']
+                while not trade_date_found and tries <=4:                                        
+                    try:                        
+                        price = prices[trade_date]
+                        trade_date_found = True
+                        row.append(trade_date)
+                        row.append(price)
+                    except KeyError as e:
+                        trade_date = unicode((parse(trade_date) + timedelta(days=1)).date())
+                        pass
+                    tries +=1
+                # row.append(1 if tries>1 else 0) #mark entries where trade_date was adjusted if it falls on a holiday        
 
             except Exception as e:
                 print (str(e) + '\n' + str(s)) 
