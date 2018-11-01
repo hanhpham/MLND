c3d9f2b224c498f476fd705ae8cd3c2216a13440
Hanh Pham <pham.hanh.m@gmail.com>
2018-10-31 20:39:49 -0700

    fixed issue where entries with unresolved trade dates get printed to output file; added range check on trade dates

--- projects/capstone/features_gather.py
+++ projects/capstone/features_gather.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import json
+from collections import OrderedDict
 from dateutil.parser import parse
 from datetime import datetime, date, timedelta
 
@@ -10,7 +11,7 @@ Generate CSV containing analyzed and raw article data, along with stock prices, 
 
 """
 
-prices = {} # Date : Opening price
+prices = OrderedDict() # Date : Opening price
 with open ('./data/GOOG.csv') as prices_csv:
     reader = csv.reader(prices_csv)
     header = reader.next()
@@ -19,21 +20,23 @@ with open ('./data/GOOG.csv') as prices_csv:
         prices[row[date_index]] = row[price_index]
 prices_csv.close()
 
+# todo: compute day-to-day percent changes
+
 outfile = open('./data/ny_times/google/news_features.csv', "w")
 writer = csv.writer(outfile, delimiter=',')
 
-years = [2016, 2015, 2014, 2013, 2012, 2011,
-         2010, 2009, 2008, 2007, 2006, 2005, 2004]
+years = [2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004]
 months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-# years = [2016]
-# months = [12]
 
+# desired time window for articles
+DATE_START = date(2004, 8, 19)
+DATE_END = date(2015, 9, 30)
 
 writer.writerow(['id', 'pub_date', 'headline_senti', 'summary_senti',
                  'headline_summary_senti', 'lead_paragraph_senti', 'keyword_in_headline', 
                  'keyword_in_summary', #'keyword_is_major', 'keyword_rank',
                  'keyword_org_rank_alt', 'section_name', 'type_of_material',
-                 'print_page', 'word_count', 'trade_date', 'price'])
+                 'print_page', 'word_count', 'trade_date', 'price', 'percent_change'])
 
 articles_count = 0
 for year in years:
@@ -54,16 +57,40 @@ for year in years:
         
         articles = raw_articles_dict['response']['docs']
 
+        previous_trade_date, change = None, None
         for article in articles:
             articles_count +=1
             row = []
-            try:
+            try:             
                 id = unicode(article['_id'])
                 try:
                     s = sentiments_dict[id]
                 except KeyError:
                     # article doesn't exist in sentiments file probably due missing critical fields, e.g. both 'snippet' and 'abstract' --> skip
-                    continue    
+                    continue
+
+                # if trade date not found in Yahoo's historical stock data, most likely the date fell on a holiday, use the next available trade date
+                trade_date_found, tries = False, 0
+                trade_date, price = s['trade_date'], None
+                while not trade_date_found and tries <= 4:
+                    try:
+                        price = prices[trade_date]
+                        trade_date_found = True
+                        # row.append(trade_date)
+                        # row.append(price)
+
+                    except KeyError as e:
+                        trade_date = unicode(
+                            (parse(trade_date) + timedelta(days=1)).date())
+                        pass
+                    tries += 1
+
+                # check that article's associated trade-date is in valid range
+                trade_date_ = parse(trade_date).date()
+                if trade_date_ < DATE_START or trade_date_ > DATE_END:
+                    # invalid -> skip
+                    continue 
+
                 row.append(id)
                 row.append(s['pub_date'])              
                 row.append(s['headline_senti'])
@@ -90,21 +117,8 @@ for year in years:
                 row.append(article['type_of_material']) 
                 row.append(article['print_page'] if article['print_page'] is not None else 0)
                 row.append(article['word_count'])
-                
-                # if trade date not found in Yahoo's historical stock data, most likely the date fell on a holiday, use the next available trade date 
-                trade_date_found, tries = False, 0
-                trade_date = s['trade_date']
-                while not trade_date_found and tries <=4:                                        
-                    try:                        
-                        price = prices[trade_date]
-                        trade_date_found = True
-                        row.append(trade_date)
-                        row.append(price)
-                    except KeyError as e:
-                        trade_date = unicode((parse(trade_date) + timedelta(days=1)).date())
-                        pass
-                    tries +=1
-                # row.append(1 if tries>1 else 0) #mark entries where trade_date was adjusted if it falls on a holiday        
+                row.append(trade_date)
+                row.append(price)      
     
                 writer.writerow(row)
             except KeyError as e:
